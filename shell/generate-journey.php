8366d450954772a9ff1b83b94c82f8b712c1c365
aphroz <aphroz@gmail.com>
2015-10-24 16:49:09 +0200

    Orders, Total sales and top 5 products export script

--- shell/generate-journey.php
+++ shell/generate-journey.php
@@ -6,14 +6,36 @@
  * Date: 24.10.2015
  * Time: 14:22
  */
-require "../Mage.php";
+ini_set('display_errors', 1);
+require "../app/Mage.php";
 $app = Mage::app('');
 
 $salesModel=Mage::getModel("sales/order");
 $salesCollection = $salesModel->getCollection();
+$productsData = array();
+
 
 foreach($salesCollection as $order)
 {
-    $orderId= $order->getIncrementId();
-    echo $orderId;
+    $items = $order->getAllVisibleItems();
+    foreach($items as $item){
+        $productsData[$item->getSku()] += $item->getQtyOrdered();
+    };
+
+    $data['sales'] += $order->getGrandTotal();
+    $data['orders']++;
+
 }
+
+sort($productsData);
+
+$topProducts = array_slice($productsData,-5, 5);
+
+$i = 1;
+
+foreach($topProducts as $product){
+    $data['product_'.$i] =$product;
+    $i++;
+}
+
+echo json_encode($data);
